4b18701cd48f8a36ad8f217aea0308f00daaf8c3
divyagautam001 <divyashreegautam@gmail.com>
2024-03-01 21:05:01 +0530

    add template in required format with default values

--- src/main/java/com/freightfox/pdfgenerator/controller/WebController.java
+++ src/main/java/com/freightfox/pdfgenerator/controller/WebController.java
@@ -2,6 +2,7 @@ package com.freightfox.pdfgenerator.controller;
 
 import com.freightfox.pdfgenerator.entity.BuyerSellerDetails;
 import com.freightfox.pdfgenerator.service.ConvertToPdfService;
+import com.freightfox.pdfgenerator.service.DataService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @RestController
 @RequestMapping("/api")
@@ -24,10 +28,14 @@ public class WebController {
     @Autowired
     private SpringTemplateEngine springTemplateEngine;
 
+    @Autowired
+    private DataService dataService;
+
     @PostMapping("/generate-pdf")
     public String generatePdf(@RequestBody BuyerSellerDetails details){
         logger.info(details.toString());
-        String finalHtml = springTemplateEngine.process("buyer_seller",new Context());
+        Context context = dataService.setBuyerSellerData(details);
+        String finalHtml = springTemplateEngine.process("buyer_seller",context);
         convertToPdfService.htmlToPdf(finalHtml);
         return "success";
     }
---
+++ src/main/java/com/freightfox/pdfgenerator/service/DataService.java
@@ -0,0 +1,15 @@
+package com.freightfox.pdfgenerator.service;
+
+import com.freightfox.pdfgenerator.entity.BuyerSellerDetails;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+
+@Service
+public class DataService {
+
+    public Context setBuyerSellerData(BuyerSellerDetails details){
+        Context context = new Context();
+        context.setVariable("details",details);
+        return context;
+    }
+}
