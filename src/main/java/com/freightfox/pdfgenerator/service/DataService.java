f32daaad0e6f2cf1e86c451d63f011bf81c09f5b
divyagautam001 <divyashreegautam@gmail.com>
2024-03-01 22:44:39 +0530

    add mapping of input from controller to the thymeleaf template

--- src/main/java/com/freightfox/pdfgenerator/service/DataService.java
+++ src/main/java/com/freightfox/pdfgenerator/service/DataService.java
@@ -1,11 +1,13 @@
 package com.freightfox.pdfgenerator.service;
 
 import com.freightfox.pdfgenerator.entity.BuyerSellerDetails;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 
 @Service
 public class DataService {
+
 
     public Context setBuyerSellerData(BuyerSellerDetails details){
         Context context = new Context();
